08de7c16c5f6f78f0b3fc503b5ea94bcc38f8a26
MaksFank <mytestgit@admin.com>
2026-01-18 23:15:03 +0300

    cheange calculator function

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 function update() {
-    let money = +document.querySelector('.money').value
-    let year = +document.querySelector('.year').value
-    let result = money + money * 0.12 * year
-    document.querySelector('.result').textContent = result
+    let initial = +document.querySelector('.initial-price').value;
+    let final = +document.querySelector('.final-price').value;
+
+    if (initial === 0 || isNaN(initial) || isNaN(final) || initial < 0 || final < 0) {
+        document.querySelector('.result').textContent = 'Ошибка';
+        return;
+    }
+
+    let markup = ((final - initial) / initial * 100);
+    document.querySelector('.result').textContent = Math.round(markup) + '%';
 }
 
 update()
 
-document.querySelector('.money').addEventListener('input', update)
+document.querySelector('.initial-price').addEventListener('input', update)
 
-document.querySelector('.year').addEventListener('input', update)
+document.querySelector('.final-price').addEventListener('input', update)
